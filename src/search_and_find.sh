6638e042f8b886c84a90c90bab33c8097abafb04
Tavis Rudd <tavis@damnsimple.com>
2014-06-11 14:07:03 -0700

    fix some sloppiness in pyfind

--- src/search_and_find.sh
+++ src/search_and_find.sh
@@ -6,7 +6,11 @@ killpycs(){
 }
 
 pyfind() {
-  local x="$(python -c "import $1; print $1.__file__" | sed 's/\.pyc$/\.py/')"
-  [[ $? -ne 0 ]] && return $?
-  grep -q "__init__.py$" <<< "$x" && echo $(dirname "$x") || echo "$x"
+  local match
+  if match="$(python -c "import $1; print $1.__file__")"; then
+      match="${match/.pyc/.py}"
+      grep -q '__init__.py$' <<< "$match" && dirname "$match" || echo "$match"
+  else 
+      return 1
+  fi
 }
